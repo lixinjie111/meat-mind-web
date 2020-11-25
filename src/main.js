50d10b0e242902099dbe48e6b743b12636cbc13e
hd <danmn1230@sina.com>
2020-11-25 15:01:45 +0800

    整理

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ViewUI from 'view-design'
+import 'view-design/dist/styles/iview.css'
 import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.css'
 import echarts from 'echarts'
 
-Vue.config.productionTip = false;
-Vue.prototype.$echarts = echarts;
-Vue.use(ViewUI);
+Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
+Vue.use(ViewUI)
 Vue.use(Antd)
 
 new Vue({
