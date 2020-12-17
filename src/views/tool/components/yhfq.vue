c8830f6bc4df77c4a7fe5538044bee019e1d891e
lyd272230 <lyd272230@163.com>
2020-12-17 19:08:37 +0800

    分析工具

--- src/views/tool/components/yhfq.vue
+++ src/views/tool/components/yhfq.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="yhfq">
-      <img src="../../../static/img/tool/yhfq.png" alt="">
+      <div class="list" v-if="page1" @click="toPage2">
+        <img src="../../../static/img/tool/yhfq/list1.png" alt="">
+        <div class="page2"></div>
+      </div>
+      <div class="list" v-if="page2" @click="toPage1">
+        <img src="../../../static/img/tool/yhfq/list2.png" alt="">
+        <div class="page1"></div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name:"yhfq",
+  data(){
+    return {
+      page1:true,
+      page2:false
+    }
+  },
+  methods:{
+    toPage1(){
+      this.page1=true
+      this.page2=false
+    },
+    toPage2(){
+      this.page1=false
+      this.page2=true
+    }
+  }
 }
 </script>
 
@@ -14,9 +37,28 @@ export default {
 .yhfq{
   width: 100%;
   height: 100%;
-  img{
-    width: 100%;
-    // height: 100%;
+  .list{
+    position: relative;
+    img{
+      width: 100%;
+      // height: 100%;
+    }
+    .page1{
+      position: absolute;
+      width: 80px;
+      height: 46px;
+      right: 46px;
+      bottom: 200px;
+      cursor: pointer;
+    }
+    .page2{
+      position: absolute;
+      width: 80px;
+      height: 46px;
+      right: 50px;
+      bottom: 42px;
+      cursor: pointer;
+    }
   }
 }
 </style>
