0ef94353b9a7ec1b000624ff6ba93f886ac2717d
fj <660514@gdpr.com>
2021-01-27 19:08:20 +0800

    111

--- src/main.js
+++ src/main.js
@@ -6,14 +6,16 @@ import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
 import echarts from 'echarts'
 import lxjData from "./utils/lxjData.js";
+import fjData from "./utils/fjData.js";
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = echarts;
 Vue.prototype.$lxjData = lxjData;
+Vue.prototype.$fjData = fjData;
 Vue.use(ViewUI)
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -2,43 +2,49 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/index.vue'
 import Login from "../views/login.vue"
-import home from "../views/home/router"//首页
-import analysisTool from "../views/tool/router"//分析工具
+import home from "../views/home/router" //首页
+import analysisTool from "../views/tool/router" //分析工具
 import portraitSystem from "../views/protrait/router" //画像系统
 // import intelligentDecision from "../views/decision/router"//智能决策
-import dataCenter from "../views/datacenter/router"//数据管理
-import businessAnalysis from "../views/analysis/router"//经营分析
-import barView from "../components/echarts/barView"//经营分析
+import dataCenter from "../views/datacenter/router" //数据管理
+import businessAnalysis from "../views/analysis/router" //经营分析
+import barView from "../components/echarts/barView" //经营分析
+import pieView from "../components/echarts/pieView" //经营分析
 
 Vue.use(VueRouter)
 
 const routes = [
-  ...home,
-  // ...intelligentDecision,
-  ...businessAnalysis,
-  ...portraitSystem,
-  ...analysisTool,
-  ...dataCenter,
-  {
-    path: '/',
-    name: 'index',
-    component: Index
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: Login
-  },
-  {
-    path: '/barView',
-    name: 'barView',
-    component: barView
-  }
+    ...home,
+    // ...intelligentDecision,
+    ...businessAnalysis,
+    ...portraitSystem,
+    ...analysisTool,
+    ...dataCenter,
+    {
+        path: '/',
+        name: 'index',
+        component: Index
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/barView',
+        name: 'barView',
+        component: barView
+    },
+    {
+        path: '/pieView',
+        name: 'pieView',
+        component: pieView
+    }
 ]
 
 const router = new VueRouter({
-  routes,
-  // mode:'history',
+    routes,
+    // mode:'history',
 })
 
 export default router
