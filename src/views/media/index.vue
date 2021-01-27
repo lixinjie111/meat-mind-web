ec4abf2f72fa6f0d37f6db34a10f4541d06ccbef
hd <danmn1230@sina.com>
2021-01-27 18:01:46 +0800

    dashboard

--- src/views/media/index.vue
+++ src/views/media/index.vue
@@ -1,39 +1,23 @@
 <template>
-  <div class="media-protrait">
-      <!-- 媒介画像 -->
-        <Side></Side>
-        <div class="container">
-          <Header :target="'媒介画像'"></Header>
-          <div class="show-content"></div>
+    <DefaultPage title="媒介画像">
+        <div class="media-protrait">
+            <div></div>
         </div>
-  </div>
+    </DefaultPage>
 </template>
 
 <script>
-import Side from "../../components/Side"
-import Header from "../../components/DefaultHeader"
-export default {
-    name:"index",
-    components:{Side,Header}
-}
+    import DefaultPage from "../../layouts/DefaultPage";
+
+    export default {
+        name: "index",
+        components: {DefaultPage}
+    }
 </script>
+
 
 <style scoped lang="scss">
     .media-protrait {
-        display: flex;
 
-        .container {
-            display: flex;
-            flex-direction: column;
-            width: calc(100vw - 240px);
-
-            .show-content{
-                display: flex;
-                flex-direction: column;
-                width: 100%;
-                height: calc(100vh - 80px);
-                overflow-y:scroll;
-            }
-        }
-    } 
+    }
 </style>
