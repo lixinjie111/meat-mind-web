83ff6c11e4a3cfb16e0cf45892a58d51326ae00a
hd <danmn1230@sina.com>
2021-02-02 15:42:46 +0800

    dashboard

--- src/components/dashboard/BusinessOwner.vue
+++ src/components/dashboard/BusinessOwner.vue
@@ -8,6 +8,10 @@
                 <BusinessManage></BusinessManage>
             </TabPane>
         </Tabs>
+        <div class="report-btn">
+            <img src="../../assets/img/dashboard/downLoad.png" class="btn-download"/>
+            <span class="btn-txt">一键生成诊断报告</span>
+        </div>
     </div>
 </template>
 
@@ -26,6 +30,32 @@
 
 <style scoped lang="scss">
     .business-owner {
+        position: relative;
         padding-top: 20px;
+
+        .report-btn {
+            position: absolute;
+            right: 0;
+            top: 8px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 169px;
+            height: 40px;
+            background: linear-gradient(139deg, #F0F3F6 0%, #FFFFFF 100%);
+            box-shadow: 4px 4px 16px 0px rgba(55, 84, 170, 0.16), -4px -4px 16px 0px #FFFFFF, 1px 1px 0px 0px #FFFFFF;
+            border-radius: 24px;
+            font-size: 14px;
+            font-family: PingFangSC-Medium, PingFang SC;
+            font-weight: 500;
+            color: #242F57;
+
+            .btn-download {
+                display: block;
+                width: 14px;
+                height: 14px;
+                margin-right: 3px;
+            }
+        }
     }
 </style>
