5fb35a53341604040b7a4714bcc8bbcea8d9098f
hd <danmn1230@sina.com>
2021-02-03 17:09:08 +0800

    登录弹框修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -39,28 +39,31 @@
                     ></vCard0>
                 </div>
             </div>
-            <Modal title="欢迎使用智慧营销"
-                   v-model="showModal"
+            <Modal v-model="showModal"
                    footer-hide
                    :mask-closable="false"
-                   class-name="vertical-center-modal">
+                   :closable="false"
+                   class-name="login-modal">
                 <div class="middle">
+                    <div class="title">欢迎使用智慧营销</div>
+                    <div class="sub-title">请选择您的身份</div>
                     <div class="select">
-                        请选择您的身份
-                        <Select v-model="model1" @on-change="change">
-                            <Option v-for="item in optList" :value="item.value" :key="item.value">{{ item.label }}
-                            </Option>
-                        </Select>
+                        <div @click="role = 0" :class="role == 0 ? 'active' : ''">
+                            <img src="../static/img/header/oval@2x.png" alt="">
+                            <p>分析师</p>
+                        </div>
+                        <div @click="role = 1" :class="role == 1 ? 'active' : ''">
+                            <img src="../static/img/header/oval@2x.png" alt="">
+                            <p>企业主</p>
+                        </div>
+                        <div @click="role = 2" :class="role == 2 ? 'active' : ''">
+                            <img src="../static/img/header/oval@2x.png" alt="">
+                            <p>数据管理员</p>
+                        </div>
                     </div>
-                    <div>*根据您的身份，我们为您准备了不同的展示重点</div>
-                </div>
-                <div class="footer">
-                    <div class="confirm" @click="confirm">确认</div>
+                    <div :class="[role >= 0 ? 'active' : '','confirm']" @click="confirm">确认</div>
                 </div>
             </Modal>
-            <div class="second-step" v-show="showImg">
-                <img src="../static/img/login/welcome@2x.png" alt="" @click="jump">
-            </div>
         </div>
     </DefaultPage>
 </template>
@@ -75,22 +78,7 @@
         data() {
             return {
                 showModal: true,
-                model1: "分析师",
-                optList: [
-                    {
-                        value: '分析师',
-                        label: '分析师'
-                    },
-                    {
-                        value: '企业主',
-                        label: '企业主'
-                    },
-                    {
-                        value: '数据管理员',
-                        label: '数据管理员'
-                    }
-                ],
-                showImg: false,
+                role: -1, //分析师-0，企业主-1，数据管理员-2
                 guideList: [
                     {
                         tilIcon: require("../assets/img/dashboard/newGif.png"),
@@ -144,25 +132,13 @@
             }
         },
         methods: {
-            change(val) {
-                this.model1 = val
-            },
             confirm() {
-                this.showModal = false
-                this.showImg = true
-            },
-            jump() {
-                this.showImg = false
-                if (this.model1 == "企业主") {
-                    this.$router.push({name: "dashboard"})
-                    localStorage.setItem("dashboard", 1)
-                } else if (this.model1 == "数据管理员") {
-                    this.$router.push({name: "dashboard"})
-                    localStorage.setItem("dashboard", 2)
-                } else {
-                    this.$router.push({name: "dashboard"})
-                    localStorage.setItem("dashboard", 0)
+                if (this.role < 0) {
+                    this.$Message.error('请选择您的身份');
+                    return false;
                 }
+                this.$router.push({name: "dashboard"});
+                localStorage.setItem("dashboard", this.role);
             }
         }
     }
@@ -173,77 +149,103 @@
         background-color: rgba(0, 0, 0, .6) !important;
     }
 
-    .vertical-center-modal {
+    .login-modal {
         display: flex;
         align-items: center;
         justify-content: center;
 
         .ivu-modal {
             top: 0;
-            width: 684px !important;
-            min-height: 353px;
-            background: #ffffff;
-            border-radius: 4px;
+            width: 900px !important;
+            background: #FFFFFF;
+            box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            border: 1px solid #EAEDF7;
 
             .ivu-modal-content {
-                min-height: 353px;
-
-                .ivu-modal-close {
-                    top: 16px;
-                    right: 16px;
-                }
-
-                .ivu-modal-header {
-                    padding: 14px 32px;
-                    border-bottom: 1px solid #f2f3f4;
-
-                    .ivu-modal-header-inner {
-                        height: 29px;
-                        font-size: 21px;
-                        font-family: PingFangSC-Semibold, PingFang SC;
-                        font-weight: 600;
-                        color: #25272b;
-                        line-height: 29px;
-                        //   text-align: center;
-                    }
-                }
-
                 .ivu-modal-body {
-                    padding: 0 32px 64px;
+                    padding: 56px;
 
                     .middle {
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        flex-direction: column;
-                        height: 200px;
-
-                        .select {
-                            margin: 20px 0 40px;
-
-                            .ivu-select {
-                                width: 200px;
-                            }
-                        }
-                    }
-
-                    .footer {
-                        width: 100px;
                         margin: 0 auto;
+                        width: 600px;
                         text-align: center;
 
-                        > div {
-                            height: 32px;
-                            padding: 6px 26px;
-                            font-size: 14px;
-                            border-radius: 4px;
-                            cursor: pointer;
-                            vertical-align: top;
+                        .title {
+                            font-size: 24px;
+                            font-family: PingFangSC-Medium, PingFang SC;
+                            font-weight: 500;
+                            color: #242F57;
+                        }
+
+                        .sub-title {
+                            margin: 60px 0 30px;
+                            font-size: 16px;
+                            font-family: PingFangSC-Medium, PingFang SC;
+                            font-weight: 500;
+                            color: #97A0C3;
+                        }
+
+                        .select {
+                            display: flex;
+                            justify-content: space-between;
+                            align-content: center;
+                            height: 184px;
+
+                            > div {
+                                width: 160px;
+                                height: 160px;
+                                box-shadow: 6px 6px 12px 0px rgba(99, 110, 149, 0.2), -6px -6px 12px 0px #FFFFFF;
+                                border: 2px solid #FFFFFF;
+                                border-radius: 50%;
+                                cursor: pointer;
+
+                                > img {
+                                    width: 156px;
+                                    height: 156px;
+                                    border-radius: 50%;
+                                }
+
+                                > p {
+                                    font-size: 16px;
+                                    font-family: PingFangSC-Medium, PingFang SC;
+                                    font-weight: 500;
+                                    color: #97A0C3;
+                                }
+
+                                &.active {
+                                    box-shadow: 8px 8px 20px 0px rgba(99, 110, 149, 0.15), -6px -6px 12px 0px rgba(255, 255, 255, 0.5);
+                                    border: 2px solid #2373FF;
+
+                                    > p {
+                                        color: #2373FF;
+                                    }
+                                }
+                            }
                         }
 
                         .confirm {
-                            color: #ffffff;
-                            background: #278fe0;
+                            margin: 62px auto 4px;
+                            width: 120px;
+                            height: 48px;
+                            line-height: 48px;
+                            background: linear-gradient(139deg, #F0F3F6 0%, #FFFFFF 100%);
+                            box-shadow: 4px 4px 16px 0px rgba(55, 84, 170, 0.16), -4px -4px 16px 0px #FFFFFF, 1px 1px 2px 0px #FFFFFF;
+                            border-radius: 32px;
+                            font-size: 22px;
+                            font-family: PingFangSC-Regular, PingFang SC;
+                            font-weight: 400;
+                            color: #C6CBDE;
+                            text-align: center;
+                            cursor: pointer;
+
+                            &.active {
+                                background: #1B74FF;
+                                border-radius: 24px;
+                                font-family: PingFangSC-Medium, PingFang SC;
+                                font-weight: 500;
+                                color: #FFFFFF;
+                            }
                         }
                     }
                 }
@@ -406,26 +408,6 @@
                 display: flex;
                 justify-content: space-between;
                 flex-wrap: wrap;
-            }
-        }
-
-        .second-step {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            position: fixed;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            left: 0;
-            // background: #f5f5f5;
-            background: rgba(0, 0, 0, 0.6);
-            z-index: 1000;
-
-            img {
-                width: 862px;
-                height: 554px;
-                cursor: pointer;
             }
         }
     }
