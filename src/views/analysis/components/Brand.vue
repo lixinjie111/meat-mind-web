f310d96ca83d9755236e11966dd44b6c5359fc20
lyd272230 <lyd272230@163.com>
2021-02-07 12:41:27 +0800

    经营分析

---
+++ src/views/analysis/components/Brand.vue
@@ -0,0 +1,17 @@
+<template>
+  <BasePage>
+    <img src="../../../assets/img/analysis/brand.png" alt="">
+  </BasePage>
+</template>
+
+<script>
+import BasePage from "../base/BasePage"
+export default {
+ name:"Brand",
+ components:{BasePage}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/analysis/components/Channel.vue
@@ -0,0 +1,15 @@
+<template>
+  <BasePage>
+  <img src="../../../assets/img/analysis/channel.png" alt=""></BasePage>
+</template>
+
+<script>
+import BasePage from "../base/BasePage"
+export default {
+ name:"Channel",
+ components:{BasePage}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
