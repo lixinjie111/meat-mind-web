6bed1cb65c9d1ca30fe8fef433a85d2fff15d253
hd <danmn1230@sina.com>
2021-02-07 21:19:11 +0800

    返回顶部修改

--- src/components/DefaultHeader.vue
+++ src/components/DefaultHeader.vue
@@ -32,7 +32,7 @@
         <img class="triangle" src="../assets/img/header/triangle@2x.png" alt="">
       </div>
     </div>
-    <BackTop></BackTop>
+    <BackTop bottom="8" right="8"></BackTop>
   </div>
 </template>
 
