a0a554db3ea09c4bb281ee26df8a95af1c2c3409
zjj <660403@gdpr.com>
2021-02-25 15:12:56 +0800

    增加矩形关系图与气泡关系图组件

---
+++ src/components/echarts/common/zjjTreemap.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="echarts-box" :id="id"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    myData: {
+      default: () => {
+        return [];
+      },
+    },
+    id: {
+      type: String,
+      default: () => {
+        return "";
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    this.initEcharts();
+  },
+  methods: {
+    initEcharts() {
+      let _option = this.defaultOption();
+      let myChart = this.$echarts.init(document.getElementById(this.id));
+      myChart.setOption(_option);
+      window.addEventListener("resize", () => {
+        myChart.resize();
+      });
+    },
+    defaultOption() {
+      var option = (option = {
+        tooltip: {
+          show: true,
+        },
+        series: [
+          {
+            type: "treemap",
+            data:this.myData,
+          },
+        ],
+      });
+      return option;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.echarts-box {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -533,7 +533,7 @@
         </div>
       </div>
       <div class="mark-detail-anchor">
-      <Anchor show-ink offset-top="64">
+      <Anchor show-ink :offset-top="64">
             <AnchorLink href="#jichuhuax_area_container" title="基础画像" />
             <AnchorLink href="#xwfx_area_container" title="行为分析" />
             <AnchorLink href="#xzfx_area_container" title="心智分析" />
