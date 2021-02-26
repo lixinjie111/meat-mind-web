0f66cd6201ddc0075d050b87f81f4918493082c6
lixinjie <15110074982@139.com>
2021-02-26 16:10:01 +0800

    优化

--- src/components/echarts/barView.vue
+++ src/components/echarts/barView.vue
@@ -37,6 +37,12 @@
 			<barL id="box24" :colorList="$lxjData.colorList" :myData="$lxjData.box24Data"></barL>
 		</div>
 		<div class="box">
+			<barL id="box240" :colorList="$lxjData.colorList" :myData="$lxjData.box240Data"></barL>
+		</div>
+		<div class="box">
+			<barL id="box241" :colorList="$lxjData.colorList" :myData="$lxjData.box241Data"></barL>
+		</div>
+		<div class="box">
 			<barL id="box25" :colorList="$lxjData.colorList" :myData="$lxjData.box25Data"></barL>
 		</div>
 		<div class="box">
@@ -52,9 +58,6 @@
 			<barL id="box29" :colorList="$lxjData.colorList" :myData="$lxjData.box29Data"></barL>
 		</div>
 		<div class="box">
-			<barM id="box3" :colorList="$lxjData.colorList" :myData="$lxjData.box3Data"></barM>
-		</div>
-		<div class="box">
 			<barM id="box31" :colorList="$lxjData.colorList" :myData="$lxjData.box31Data"></barM>
 		</div>
 		<div class="box">
@@ -62,6 +65,27 @@
 		</div>
 		<div class="box">
 			<barM id="box4" :colorList="$lxjData.colorList" :myData="$lxjData.box4Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box40" :colorList="$lxjData.colorList" :myData="$lxjData.box40Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box41" :colorList="$lxjData.colorList" :myData="$lxjData.box41Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box42" :colorList="$lxjData.colorList" :myData="$lxjData.box42Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box43" :colorList="$lxjData.colorList" :myData="$lxjData.box43Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box44" :colorList="$lxjData.colorList" :myData="$lxjData.box44Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box45" :colorList="$lxjData.colorList" :myData="$lxjData.box45Data"></barM>
+		</div>
+		<div class="box">
+			<barM id="box46" :colorList="$lxjData.colorList" :myData="$lxjData.box46Data"></barM>
 		</div>
 		<div class="box">
 			<cLine id="box5" :colorList="$lxjData.colorList" :myData="$lxjData.box5Data"></cLine>
@@ -112,6 +136,12 @@
 			<lineM id="box65" :colorList="$lxjData.colorList" :myData="$lxjData.box65Data"></lineM>
 		</div>
 		<div class="box">
+			<lineM id="box67" :colorList="$lxjData.colorList" :myData="$lxjData.box67Data"></lineM>
+		</div>
+		<div class="box">
+			<lineM3 id="box68" :colorList="$lxjData.colorList" :myData="$lxjData.box68Data"></lineM3>
+		</div>
+		<div class="box">
 			<barC id="box7" :colorList="$lxjData.colorList" :myData="$lxjData.box7Data"></barC>
 		</div>
 		<div class="box">
@@ -145,7 +175,45 @@
 			<barHM id="box114" :colorList="$lxjData.colorList" :myData="$lxjData.box114Data"></barHM>
 		</div>
 		<div class="box">
+			<div class="box1">
+				<barHM id="box1141" :colorList="$lxjData.colorList" :myData="$lxjData.box1141Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box1142" :colorList="$lxjData.colorList" :myData="$lxjData.box1142Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box1143" :colorList="$lxjData.colorList" :myData="$lxjData.box1143Data"></barHM>
+			</div>
+		</div>
+		<div class="box">
+			<div class="box1">
+				<barHM id="box11410" :colorList="$lxjData.colorList" :myData="$lxjData.box11410Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box11420" :colorList="$lxjData.colorList" :myData="$lxjData.box11420Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box11430" :colorList="$lxjData.colorList" :myData="$lxjData.box11430Data"></barHM>
+			</div>
+		</div>
+		<div class="box">
+			<div class="box1">
+				<barHM id="box11411" :colorList="$lxjData.colorList" :myData="$lxjData.box11411Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box11421" :colorList="$lxjData.colorList" :myData="$lxjData.box11421Data"></barHM>
+			</div>
+			<div class="box1">
+				<barHM id="box11431" :colorList="$lxjData.colorList" :myData="$lxjData.box11431Data"></barHM>
+			</div>
+		</div>
+		<div class="box">
 			<pieP id="box115" :colorList="$lxjData.colorList"></pieP>
+		</div>
+
+
+		<div class="box">
+			<barLine id="box101" :colorList="$lxjData.colorList" :myData="$lxjData.box101Data"></barLine>
 		</div>
         
     </div>
@@ -160,6 +228,7 @@
 	import lineO from './common/lineO';
 	import lineS from './common/lineS';
 	import lineM from './common/lineM';
+	import lineM3 from './common/lineM3';
 	import barC from './common/barC';
 	import barCH from './common/barCH';
 	import barLine from './common/barLine';
@@ -170,7 +239,7 @@
 	import pieP from './common/pieP';
     export default {
 		components: {
-		  barEcharts,barL,barM,cLine,lineM,barC,barLine,barT,lineS,funnel,barCH,barHM,lineO,lineSp,pieP
+		  lineM3,barEcharts,barL,barM,cLine,lineM,barC,barLine,barT,lineS,funnel,barCH,barHM,lineO,lineSp,pieP
 		},
 		created(){
 			console.log(this.$lxjData)
@@ -187,6 +256,9 @@
 			height: 300px;
 			margin: 3px;
 			border: 1px solid #f00;
+			.box1{
+				height: 30%;
+			}
 		}
 	}
 	
