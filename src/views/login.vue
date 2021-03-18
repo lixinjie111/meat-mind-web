88930017765d7fe9085abbec405a672bafcf93fc
hd <danmn1230@sina.com>
2021-03-18 18:38:54 +0800

    登录页，分析师页面修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,62 +1,71 @@
 <template>
     <div class="login-box">
-        <div class="nav">
-            <p class="nav-title">Dashboard</p>
-            <div class="btn_container">
-                <img :src="downLoad" class="btn_download" />
-                <span class="btntxt">一键生成报告</span>
-            </div>
+        <div class="header-nav">
+            <p>Dashboard</p>
         </div>
-        <div class="welcome">
-            <img class="user" src="../static/img/header/oval@2x.png" alt="" />
-            <div class="user_intro_container">
-                <p>早安，分析师，欢迎使用 Meta-Mind-Marketing！</p>
-                <p>
-                    轻松上手分析模型，探索不同业务中的关键行为，洞察指标背后的增长点。
-                </p>
+        <div class="wel_guid_container">
+            <div class="welcome">
+                <img class="user" src="../assets/img/login/head1.png" alt=""/>
+                <div class="user_intro_container">
+                    <div class="til_img_container">
+                        <span>早安，分析师，欢迎使用 Meta-Mind-Marketing！</span>
+                        <Poptip popper-class="saas-poptip small" placement="right-start">
+                            <i class="iconfont iconguanyu tip-icon"></i>
+                            <div slot="content">
+                                <p><i class="iconfont iconwodeshuju"></i>迅速掌握各项数据情报</p>
+                                <p><i class="iconfont iconkanban"></i>定制化的展示看板</p>
+                                <p><i class="iconfont iconfenxiline"></i>灵活可配置的分析纬度及标签</p>
+                                <p><i class="iconfont iconfenbufenxi"></i>海量新潮分析模板素材</p>
+                            </div>
+                        </Poptip>
+                    </div>
+                    <p>
+                        轻松上手分析模型，探索不同业务中的关键行为，洞察指标背后的增长点。
+                    </p>
+                </div>
+                <span class="infoIcon">
+              <div class="infoIcon-desc">
+                  <div class="infoIcon-desc-item">
+                      <i class="iconshujuguanli iconfont"></i>
+                      <span>迅速掌握各项数据情报</span>
+                  </div>
+                  <div class="infoIcon-desc-item">
+                      <i class="iconzhinengjuece iconfont"></i>
+                      <span>定制化的展示看板</span>
+                  </div>
+                  <div class="infoIcon-desc-item">
+                      <i class="iconbiaoqiantixi iconfont"></i>
+                      <span>灵活可配置的分析纬度及标签</span>
+                  </div>
+                  <div class="infoIcon-desc-item">
+                      <i class="iconzhengtigailan iconfont"></i>
+                      <span>海量新潮分析模板素材</span>
+                  </div>
+              </div>
+          </span>
             </div>
-
-            <span class="infoIcon">
-            <img :src="infoIcon" alt="" srcset="" class="infoIcon-img"/>
-            <div class="infoIcon-desc">
-                <div class="infoIcon-desc-item">
-                    <i class="iconshujuguanli iconfont"></i>
-                    <span>迅速掌握各项数据情报</span>
+            <div class="guide_container">
+                <div class="guide_title_container">
+                    <div class="guide_title_lef">
+                        <span style="margin-right: 18px">新手导航</span>
+                        <Progress :percent="45" status="active"></Progress>
+                        <span>(3/6)</span>
+                    </div>
+                    <div class="guide_title_rig" @click="exppandFn">
+                        <div class="guide_title_rig_til">{{szTxt}}</div>
+                        <div class="guide_title_rig_icon">
+                            <i class="iconfont iconup" v-if="ifShowCard"></i>
+                            <i class="iconfont icondown" v-else></i>
+                        </div>
+                    </div>
                 </div>
-                <div class="infoIcon-desc-item">
-                    <i class="iconzhinengjuece iconfont"></i>
-                    <span>定制化的展示看板</span>
+                <div class="card_container" v-show="ifShowCard">
+                    <vCard0
+                            v-for="(item, index) in guideList"
+                            :key="index"
+                            :cardData="item"
+                    ></vCard0>
                 </div>
-                <div class="infoIcon-desc-item">
-                    <i class="iconbiaoqiantixi iconfont"></i>
-                    <span>灵活可配置的分析纬度及标签</span>
-                </div>
-                <div class="infoIcon-desc-item">
-                    <i class="iconzhengtigailan iconfont"></i>
-                    <span>海量新潮分析模板素材</span>
-                </div>
-            </div>
-        </span>
-        </div>
-        <div class="guide_container">
-            <div class="guide_title_container">
-                <div class="guide_title_lef">
-                    <span style="margin-right: 4px">新手导航</span>
-                    <img :src="infoIcon" alt="" srcset="" class="infoIcon" />
-                    <Progress :percent="45" status="active"></Progress>
-                    <span>(3/6)</span>
-                </div>
-                <div class="guide_title_rig">
-                    <div class="guide_title_rig_til">收起</div>
-                    <div class="guide_title_rig_icon"></div>
-                </div>
-            </div>
-            <div class="card_container">
-                <vCard0
-                        v-for="(item, index) in guideList"
-                        :key="index"
-                        :cardData="item"
-                ></vCard0>
             </div>
         </div>
         <Modal v-model="showModal"
@@ -97,11 +106,13 @@
             return {
                 showModal: true,
                 role: -1, //分析师-0，企业主-1，数据管理员-2
+                szTxt: '收起详情',
+                ifShowCard: true,
                 guideList: [
                     {
                         tilIcon: require("../assets/img/dashboard/newGif.png"),
                         title: "领取新人礼包",
-                        iconfont: 'iconlingquxinrenlibao',
+                        iconfont: "iconxinrenlibao",
                         desc:
                             "根据您行业属性，我们为您精选出一些通用数据包及行业数据包，希望您会喜欢。",
                         statusTxt: "已完成",
@@ -145,17 +156,23 @@
                     {
                         tilIcon: require("../assets/img/dashboard/help.png"),
                         title: "帮助中心",
-                        iconfont: 'iconbianzu2',
+                        iconfont: 'iconbangzhu',
                         desc: "查看功能说明，了解智慧营销平台。",
                         statusTxt: "去查看",
                         statusIcon: require("../assets/img/dashboard/tor1.png"),
                     },
-                ],
-                infoIcon: require("../assets/img/dashboard/gdI.png"),
-                downLoad: require("../assets/img/dashboard/downLoad.png"),
+                ]
             }
         },
         methods: {
+            exppandFn() {
+                this.ifShowCard = !this.ifShowCard;
+                if (this.ifShowCard) {
+                    this.szTxt = '收起详情';
+                } else {
+                    this.szTxt = '展开详情';
+                }
+            },
             confirm() {
                 if (this.role < 0) {
                     this.$Message.error('请选择您的身份');
@@ -281,180 +298,179 @@
     .login-box {
         padding: 0 24px;
 
-        .nav {
+        .wel_guid_container {
             width: 100%;
-            padding: 16px 0 16px 24px!important;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            .btn_container {
-                padding: 14px;
-                box-sizing: border-box;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                font-size: 14px;
-                font-family: PingFangSC-Medium, PingFang SC;
-                font-weight: 500;
-                color: #242F57;
-                width: 164px;
-                height: 40px;
-                background: #FFFFFF linear-gradient(148deg, rgba(220, 225, 236, 0.2) 0%, rgba(255, 255, 255, 0.76) 100%);
-                box-shadow: 3px 4px 8px 0px rgba(36, 47, 87, 0.1);
-                border-radius: 8px;
-                border: 1px solid rgba(255, 255, 255, 0.4);
-                cursor: pointer;
-                .btn_download {
-                    display: block;
-                    width: 18px;
-                    height: 18px;
-                    margin-right: 11px;
-                }
-            }
-        }
+            background: #FFFFFF;
+            box-shadow: 4px 6px 20px 0px rgba(134, 143, 191, 0.15);
+            border-radius: 8px;
 
-        .welcome {
-            padding: 24px;
-            width: 100%;
-            height: 122px;
-            background: #ffffff;
-            box-shadow: 3px 5px 10px 0px rgba(121, 131, 168, 0.15);
-            border-radius: 12px;
-            border: 1px solid #EAEDF7;
-            position: relative;
-            .infoIcon {
-                display: block;
-                width: 16px;
-                height: 16px;
-                position: absolute;
-                top: 10px;
-                right: 10px;
-                cursor: pointer;
-                .infoIcon-img {
+            .welcome {
+                padding: 24px;
+                width: 100%;
+                height: 122px;
+                position: relative;
+
+                .infoIcon {
+                    display: block;
                     width: 16px;
                     height: 16px;
                     position: absolute;
+                    top: 10px;
+                    right: 10px;
+                    cursor: pointer;
                 }
-            }
-            .infoIcon:hover .infoIcon-desc {
-                display: block;
-            }
-            .infoIcon-desc {
-                display: none;
-                position: absolute;
-                top: 0;
-                right: 20px;
-                width: 198px;
-                height: 96px;
-                padding: 8px;
-                background: rgba(219, 234, 255, 0.94);
 
-                box-shadow: 0 0 3px 0 rgba(219, 234, 255, 0.94);
-                border-radius: 5px;
-                border: 1px solid #4D94FF;
-                .infoIcon-desc-item {
-                    font-size: 12px;
-                    font-weight: 400;
-                    color: #636E95;
-                    line-height: 20px;
-                    display: flex;
-                    .iconfont {
-                        margin-right: 8px;
+                .infoIcon:hover .infoIcon-desc {
+                    display: block;
+                }
+
+                .infoIcon-desc {
+                    display: none;
+                    position: absolute;
+                    top: 0;
+                    right: 20px;
+                    width: 198px;
+                    height: 96px;
+                    padding: 8px;
+                    background: rgba(219, 234, 255, 0.94);
+
+                    box-shadow: 0 0 3px 0 rgba(219, 234, 255, 0.94);
+                    border-radius: 5px;
+                    border: 1px solid #4D94FF;
+
+                    .infoIcon-desc-item {
+                        font-size: 12px;
+                        font-weight: 400;
+                        color: #636E95;
+                        line-height: 20px;
+                        display: flex;
+
+                        .iconfont {
+                            margin-right: 8px;
+                        }
                     }
                 }
-            }
 
-            .user {
-                margin-right: 24px;
-                width: 72px;
-                height: 72px;
-                box-shadow: 6px 6px 12px 0px rgba(99, 110, 149, 0.2),
-                -6px -6px 12px 0px #ffffff;
-                border: 1px solid #ffffff;
-                border-radius: 50%;
-            }
+                .user {
+                    margin-right: 24px;
+                    width: 72px;
+                    height: 72px;
+                    box-shadow: 6px 6px 12px 0px rgba(99, 110, 149, 0.2),
+                    -6px -6px 12px 0px #ffffff;
+                    border: 1px solid #ffffff;
+                    border-radius: 50%;
+                }
 
-            .user_intro_container {
-                display: inline-block;
-                vertical-align: top;
-                > p {
-                    font-size: 14px;
-                    font-family: PingFangSC-Regular, PingFang SC;
-                    font-weight: 400;
-                    color: #242f57;
-                    &:first-child {
-                        margin-bottom: 12px;
+                .user_intro_container {
+                    display: inline-block;
+                    vertical-align: top;
+
+                    > div {
                         font-size: 24px;
                         font-family: PingFangSC-Medium, PingFang SC;
                         font-weight: 500;
-                        color: #242f57;
+                        color: #242F57;
                     }
-                }
-            }
-        }
 
-        .guide_container {
-            width: 100%;
-            min-height: 404px;
-            background: #ffffff;
-            box-shadow: 3px 5px 10px 0px rgba(121, 131, 168, 0.15);
-            border-radius: 12px;
-            border: 1px solid #EAEDF7;
-            padding: 24px;
-            padding-bottom: 8px;
-            box-sizing: border-box;
-            margin-top: 24px;
-            .guide_title_container {
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                margin-bottom: 16px;
-                .guide_title_lef {
-                    width: 50%;
-                    display: flex;
-                    align-items: center;
-                    font-size: 16px;
-                    font-family: PingFangSC-Medium, PingFang SC;
-                    font-weight: 500;
-                    color: #242f57;
-                    .infoIcon {
-                        display: block;
-                        width: 16px;
-                        height: 16px;
-                        margin-right: 12px;
-                    }
-                    ::v-deep .ivu-progress {
-                        width: 30%;
-                    }
-                    ::v-deep .ivu-progress .ivu-progress-text {
-                        display: none;
-                    }
-                    ::v-deep .ivu-progress .ivu-progress-outer {
-                        padding-right: 10px;
-                    }
-                }
-                .guide_title_rig {
-                    display: flex;
-                    align-items: center;
-                    .guide_title_rig_til {
+                    > p {
+                        margin-top: 12px;
                         font-size: 14px;
                         font-family: PingFangSC-Regular, PingFang SC;
                         font-weight: 400;
-                        color: #636e95;
-                        margin-right: 4px;
+                        color: #7C88B1;
                     }
-                    .guide_title_rig_icon {
-                        width: 18px;
-                        height: 18px;
+
+                    .til_img_container {
+                        display: flex;
+                        align-items: baseline;
+
+                        :nth-child(1) {
+                            margin-right: 4px;
+                        }
+
+                        .infoIcon-img {
+                            width: 16px;
+                            height: 16px;
+                        }
                     }
                 }
             }
-            .card_container {
+
+            .guide_container {
                 width: 100%;
-                display: flex;
-                justify-content: space-between;
-                flex-wrap: wrap;
+                padding: 24px;
+                padding-bottom: 8px;
+                box-sizing: border-box;
+
+                .guide_title_container {
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin-bottom: 16px;
+
+                    .guide_title_lef {
+                        width: 50%;
+                        display: flex;
+                        align-items: center;
+                        font-size: 16px;
+                        font-family: PingFangSC-Medium, PingFang SC;
+                        font-weight: 500;
+                        color: #242f57;
+
+                        .infoIcon {
+                            display: block;
+                            width: 16px;
+                            height: 16px;
+                            margin-right: 12px;
+                        }
+
+                        ::v-deep .ivu-progress {
+                            width: 30%;
+                        }
+
+                        ::v-deep .ivu-progress .ivu-progress-text {
+                            display: none;
+                        }
+
+                        ::v-deep .ivu-progress .ivu-progress-outer {
+                            padding-right: 10px;
+                        }
+                    }
+
+                    .guide_title_rig {
+                        display: flex;
+
+                        &:hover {
+                            cursor: pointer;
+                        }
+
+                        .guide_title_rig_til {
+                            font-size: 14px;
+                            font-family: PingFangSC-Regular, PingFang SC;
+                            font-weight: 400;
+                            color: #636e95;
+                            margin-right: 4px;
+                        }
+
+                        .guide_title_rig_icon {
+                            width: 12px;
+                            height: 6px;
+
+                            .iconshouqi {
+                                width: 100%;
+                                height: 100%;
+                            }
+                        }
+                    }
+                }
+
+                .card_container {
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    flex-wrap: wrap;
+                }
             }
         }
     }
