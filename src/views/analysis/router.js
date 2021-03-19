1f16873e559a68ba69ae26aacb17abd5b7e0814b
hd <danmn1230@sina.com>
2021-03-19 18:27:02 +0800

    经营分析

--- src/views/analysis/router.js
+++ src/views/analysis/router.js
@@ -1,8 +1,8 @@
-const Analysis = () => import('./index')
-const Overview = () => import('./page/overview.vue')
-const User = () => import('./page/user.vue')
-const Channel = () => import('./page/channel.vue')
-const Brand = () => import('./page/brand.vue')
+const analysis = () => import('./index')
+const overview = () => import('./page/overview.vue')
+const user = () => import('./page/user.vue')
+const channel = () => import('./page/channel.vue')
+const brand = () => import('./page/brand.vue')
 const moduleName = 'business-analysis'
 
 export default [
@@ -11,34 +11,34 @@ export default [
         name: moduleName,
         redirect: {name: `${moduleName}-overview`},
         meta: {title: "经营分析", moduleName},
-        component: Analysis,
+        component: analysis,
         children: [
             {
                 path: `/${moduleName}/overview`,
                 label: "整体概览",
                 meta: {title: "整体概览", moduleName},
-                component: Overview,
+                component: overview,
                 name: `${moduleName}-overview`
             },
             {
                 path: `/${moduleName}/user`,
                 label: "用户分析",
                 meta: {title: "用户分析", moduleName},
-                component: User,
+                component: user,
                 name: `${moduleName}-user`
             },
             {
                 path: `/${moduleName}/channel`,
                 label: "渠道分析",
                 meta: {title: "渠道分析", moduleName},
-                component: Channel,
+                component: channel,
                 name: `${moduleName}-channel`
             },
             {
                 path: `/${moduleName}/brand`,
                 label: "品牌运营分析",
                 meta: {title: "品牌运营分析", moduleName},
-                component: Brand,
+                component: brand,
                 name: `${moduleName}-brand`
             }
         ]
