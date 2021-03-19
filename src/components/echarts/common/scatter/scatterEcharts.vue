b591a1cef5082ba3d7e7822146a7f7cf78a635eb
lyd272230 <lyd272230@163.com>
2021-03-19 15:02:25 +0800

    mark

--- src/components/echarts/common/scatter/scatterEcharts.vue
+++ src/components/echarts/common/scatter/scatterEcharts.vue
@@ -22,6 +22,9 @@ export default {
       let option = this.defaultOption();
       let myEchart = this.$echarts.init(document.getElementById(this.id));
       myEchart.setOption(option,true);
+      myEchart.on('click',(params)=>{
+        this.$emit('changeSeries',params.seriesName)
+      })
       window.addEventListener("resize", () => {
         myEchart.resize();
       });
@@ -30,8 +33,13 @@ export default {
       let arr = [],ary=[],warn=[]
       for(let i=0;i<31;i++){
         let num = Math.random()*(1-0.5)+0.5
+        num>0.9?num=0:null
+        num<0.6?num=0:null;
         let num2 = Math.random() - 0.5
+        num2>0&&num2<0.1?num2+=0.1:null
         let num3 = Math.random() * 0.5 -1
+        num3>-0.6?num3=0:null
+        num3<-0.9?num3=0:null
         arr.push(num)
         ary.push(num2)
         warn.push(num3)
@@ -119,73 +127,53 @@ export default {
           }
         ],
         series: [
-          // {
-          //   name: "风险1",
-          //   type: "effectScatter",
-          //   data: [
-          //     {
-          //       name: "风险",
-          //       value: [
-          //         0.963,
-          //         0.45,
-          //         "【新浪财经】和其正在凉茶巨头夹缝中迷失，同仁堂与邓老遭多重考验",
-          //       ],
-          //       itemStyle: {
-          //         color: "#FF4C60",
-          //       },
-          //       label: {
-          //         color: "#FF4C60",
-          //       },
-          //     },
-          //     {
-          //       name: "机会1",
-          //       value: [0.684, 0.8, "赢商网：邓老凉茶在京开ins风国潮店"],
-          //       itemStyle: {
-          //         color: this.colorList[1],
-          //       },
-          //       label: {
-          //         color: this.colorList[1],
-          //       },
-          //     },
-          //     {
-          //       name: "机会2",
-          //       value: [
-          //         0.222,
-          //         0.5,
-          //         "和其正发表律师函声明，要依法惩治网络舆论散播者",
-          //       ],
-          //       itemStyle: {
-          //         color: this.colorList[2],
-          //       },
-          //       label: {
-          //         color: this.colorList[2],
-          //       },
-          //     },
-          //   ],
-          //   showEffectOn: "render",
-          //   rippleEffect: {
-          //     brushType: "stroke",
-          //   },
-          //   hoverAnimation: true,
-          //   label: {
-          //       formatter: "{b}",
-          //       position: "right",
-          //       show: true,
-          //       color: "red",
-          //   },
-          //   symbolSize: 15,
-          //   itemStyle: {
-          //       color: "red",
-          //       shadowBlur: 10,
-          //       shadowColor: "#333",
-          //   },
-          //   zlevel: 1,
-          // },
+          {
+            type: "effectScatter",
+            data: [
+              {
+                // name: "风险",
+                data: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,-0.7,0,0,0,-0.8,0,0,0,0,0,0,0,0,0,0,0,0],
+                // "【新浪财经】和其正在凉茶巨头夹缝中迷失，同仁堂与邓老遭多重考验",
+                itemStyle: {
+                  color: "#FF4C60",
+                },
+                label: {
+                  color: "#FF4C60",
+                },
+              }
+              // {
+              //   name: "机会1",
+              //   value: [0.684, 0.8, "赢商网：邓老凉茶在京开ins风国潮店"],
+              // },
+              // {
+              //   name: "机会2",
+              //   value: [0.222,0.5,"和其正发表律师函声明，要依法惩治网络舆论散播者"],
+              // },
+            ],
+            // showEffectOn: "render",
+            rippleEffect: {
+              brushType: "stroke",
+            },
+            hoverAnimation: true,
+            label: {
+                formatter: "{b}",
+                position: "right",
+                show: true,
+                color: "red",
+            },
+            symbolSize: 50,
+            itemStyle: {
+                color: "red",
+                shadowBlur: 10,
+                shadowColor: "#333",
+            },
+            zlevel: 1,
+          },
           {
             type: "scatter",
             name:"机会",
             symbolSize:(value,params)=>{
-              return value*20
+              return value*16
             },
             data:arr
           },
@@ -193,7 +181,7 @@ export default {
             type: "scatter",
             name:"风险",
             symbolSize:(value,params)=>{
-              return value*10
+              return value*22
             },
             data:warn 
           },
@@ -201,7 +189,7 @@ export default {
             type: "scatter",
             name:"正常",
             symbolSize:(value,params)=>{
-              return value*30
+              return value*40
             },
             data: ary
           },
