b85fece160d424ead7edf61da65768b5cd507c12
abelzhang <abelzhang@abdstem.com>
2021-04-06 21:20:38 +0800

    调整

--- src/views/tool/components/Cause.vue
+++ src/views/tool/components/Cause.vue
@@ -9,20 +9,20 @@
       <div class="event">
         目标转化事件
         <i class="iconfont iconbangzhu"></i>
-        <Select v-model="event" class="item1">
-          <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
+        <Select v-model="app1" class="item1">
+          <Option v-for="(item,index) of $abelData.appOptions" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
       </div>
       <div class="event">
         待归因事件
         <i class="iconfont iconbangzhu"></i>
-        <Select v-model="event" class="item1">
-          <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
+        <Select v-model="app2" class="item1">
+          <Option v-for="(item,index) of $abelData.appOptions" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
       </div>
       <div class="event">
         按
-        <Select v-model="event" class="item1">
+        <Select v-model="event" class="item1" placeholder="总体">
           <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
         <span class="margin16">查看</span>
@@ -37,8 +37,8 @@
       <div class="event">
         分析模型
 
-        <Select v-model="event" class="item2">
-          <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
+        <Select v-model="funnel" class="item2">
+          <Option v-for="(item,index) of funnelNames" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
         归因窗口期
         <Select v-model="interval" class="item1">
@@ -75,43 +75,16 @@
         conditions: [{input: ''}],
         initItem: {input: ''},
         showRelation: true,
-        event: 1,
+        app1: 1,
+        app2: 0,
         conditionOptions: [
-          '总体',
-          'Distinct ID',
-          'IP',
-          'SDK版本',
-          '停留时长',
-          '元素位置',
-          '分享时的 distinct_id',
-          '分享时的层级',
-          '分享时的页面路径',
-          '前向地址',
-          '受众 ID',
-          '启动场景',
-          '失败原因',
-          '定向跳转页面',
-          '广告系列内容',
-          '广告系列名称',
-          '广告系列媒介',
-          '广告系列字词',
-          '广告系列来源',
-          '推送ID',
-          '推送内容',
-          '推送服务商',
-          '推送类型',
-          '推送通道 ID',
-          '操作系统，例如iOS',
-          '设备型号，例如iphone6',
-          '用户 ID',
-          '运营策略ID',
-          '运营计划ID',
-          '页面参数',
-          '页面名称',
-          '页面标题',
-          '崩溃原因',
         ],
-        funnelNames: ['原始页面','页面分组',],
+        funnelNames: ['首次触点归因',
+          '末次触点归因',
+          '线性归因',
+          '位置归因',
+          '时间衰减归因',],
+        funnel: 1,
         unit: 3,
         unitOptions: ['分钟','小时','天',],
         showFirstDay: true,
@@ -176,7 +149,7 @@
   }
 
   .data-container {
-    padding: 24px 24px 0;
+    padding: 0 24px;
     margin-bottom: 20px;
     background: #ffffff;
     box-shadow: 3px 3px 8px 0px rgba(166, 171, 189, 0.3);
--- src/views/tool/components/Property.vue
+++ src/views/tool/components/Property.vue
@@ -3,14 +3,14 @@
     <div class="filter-container">
       <div class="event">
         显示
-        <Select v-model="event" class="item1">
-          <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
+        <Select v-model="show" class="item1">
+          <Option v-for="(item,index) of showOptions" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
       </div>
       <div class="event">
         按
-        <Select v-model="event" class="item1">
-          <Option v-for="(item,index) of conditionOptions" :value="index+1" :key="index+1">{{item}}</Option>
+        <Select v-model="vip" class="item1">
+          <Option v-for="(item,index) of $abelData.vipTotalOptions" :value="index+1" :key="index+1">{{item}}</Option>
         </Select>
         <span class="margin16">查看</span>
       </div>
@@ -36,41 +36,44 @@
         conditions: [{input: ''}],
         initItem: {input: ''},
         showRelation: true,
-        conditionOptions: [
-          '总体',
-          'Distinct ID',
-          'IP',
-          'SDK版本',
-          '停留时长',
-          '元素位置',
-          '分享时的 distinct_id',
-          '分享时的层级',
-          '分享时的页面路径',
-          '前向地址',
-          '受众 ID',
-          '启动场景',
-          '失败原因',
-          '定向跳转页面',
-          '广告系列内容',
-          '广告系列名称',
-          '广告系列媒介',
-          '广告系列字词',
-          '广告系列来源',
-          '推送ID',
-          '推送内容',
-          '推送服务商',
-          '推送类型',
-          '推送通道 ID',
-          '操作系统，例如iOS',
-          '设备型号，例如iphone6',
-          '用户 ID',
-          '运营策略ID',
-          '运营计划ID',
-          '页面参数',
-          '页面名称',
-          '页面标题',
-          '崩溃原因',
+        showOptions: ['用户数',
+          '年龄',
+          '用户更新时间',
+          '用户邀请粉丝数',
+          '30天关注商品价格偏好',
+          '30天内全平台访问天数分布',
+          '30天内访问次数',
+          '30天商品加购次数',
+          '30天商详页访问次数',
+          '30天消费总金额',
+          '30天消费次数',
+          '7天内全平台访问天数分布',
+          '7天内访问次数',
+          '7天商品加购次数',
+          '7天商详页访问次数',
+          'APP访问时段偏好',
+          'test-行为分布',
+          '优惠券使用次数',
+          '优惠券领取次数分布',
+          '分享商品数',
+          '分享商品次数分布',
+          '发送推送次数',
+          '商城成单时段偏好',
+          '总消费次数',
+          '总消费金额',
+          '推送打开次数',
+          '末次消费时间飞鹤',
+          '标签261',
+          '用户发起搜索次数分布',
+          '累计消费金额',
+          '裂变-时段偏好',
+          '购买时间间隔',
+          '购买次数',
+          '过去30天注册用户人数',
+          '首购周期',
         ],
+        show: 1,
+        vip: 1,
         showFirstDay: true,
         date1: 1,
         date2: 1,
